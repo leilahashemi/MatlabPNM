108c087439e53f1a6e9ef69e173c89a55fa94f6f
Leila Hashemi <leila.hashemi1994@gmail.com>
2022-11-24 11:33:39 +0100

    Latest master branch including package and primary drainage!

--- main.m
+++ main.m
@@ -1,23 +1,40 @@
-clc; clear
-fileName = 'Berea';
+% Clearing the workspace
+clear
+% clearing the current window
+clc
 
-% Pressure difference (Pa)
+import quasiStatic.*
+
+
+% Getting the working directory
+currentFoldet = pwd;
+
+networkFileName = 'C1';
+% networkFileFullPath = strcat(currentFoldet, '\Input\NetworkDataFile\' , networkFileName);
+
+
+% Pressure difference
 inletPressure = 1;
 outletPressure = 0;
 
-network = Network(fileName);
+network = Network(networkFileName);
+network.calculatePorosity();
+fprintf('Porosity of the model is: %3.5f \n', network.Porosity);
 
-tic
-network.calculateNetworkProperties(inletPressure, outletPressure);
-fprintf('Porosity of the model(in percentage) is: %3.5f \n', network.Porosity);
-toc 
+% network.pressureDistribution(1,0);
+% network.calculateAbsolutePermeability();
+% fprintf('%3.5f \n', network.absolutePermeability)
+% network.calculateRelativePermeability();
+network.PrimaryDrainage();
 
-tic
-fprintf('Drainage Start\n');
-network.PrimaryDrainage(inletPressure, outletPressure);
-toc
-
-tic
-fprintf('Imbibition Start\n'); 
-network.ScoendaryImbibition(inletPressure, outletPressure); 
-toc
+press = zeros(network.numberOfNodes,1);
+x = zeros(network.numberOfNodes,1);
+index = zeros(network.numberOfNodes,1);
+for ii = 1:network.numberOfNodes
+    if network.Nodes{ii}.y_coordinate == network.Nodes{1}.y_coordinate
+    press(ii) = network.Nodes{ii}.waterPressure;
+    index(ii) = network.Nodes{ii}.index;
+    x(ii) = network.Nodes{ii}.x_coordinate;
+    end
+end
+plot(x, press, '*')
